68cda164c236d957e4c0a7c255a69ad20d73057b
David Colarusso <colarusso@users.noreply.github.com>
2025-12-14 19:30:23 -0500

    update crossword

--- js/puzzles.js
+++ js/puzzles.js
@@ -3,4 +3,5 @@ var puzzles = {
     "2025-11-24" : `https://crosshare.org/embed/jJepJtuYhSmSCRAX2nhR/305UqSnkS8eCfnettr5iC9sUtdj2`,
     "2025-12-01" : `https://crosshare.org/embed/EStXbgoYNpQ6tpN4pxYw/305UqSnkS8eCfnettr5iC9sUtdj2`,
     "2025-12-08" : `https://crosshare.org/embed/PUu437bqTXiOT2ogfR3m/305UqSnkS8eCfnettr5iC9sUtdj2`,
+    "2025-12-14" : `https://crosshare.org/embed/eEtMcmvMCQmy2j0PfoII/305UqSnkS8eCfnettr5iC9sUtdj2`,
 }
